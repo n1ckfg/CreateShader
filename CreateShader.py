1fe10d36c3b891a7b7acb9cce4b51f4f4e624175
Nick Fox-Gieg <nick@fox-gieg.com>
2013-08-13 13:15:31 -0400

    first

---
+++ CreateShader.py
@@ -0,0 +1,40 @@
+from maya.cmds import *
+from pymel.core import *
+
+def createShader(shaderType,shaderColor,useTexture):
+	#1. get selection
+	target = ls(sl=True)
+	#2. initialize shader
+	shader=shadingNode(shaderType,asShader=True)
+	#3. create a file texture node
+	if(useTexture==True):
+		file_node=shadingNode("file",asTexture=True)
+	#4. create a shading group
+	shading_group= sets(renderable=True,noSurfaceShader=True,empty=True)
+	#5. set the color
+	try:
+		setColor(shader,shaderColor)
+	except:
+		setColor(shader,(0.5,0.5,0.5))
+	#6. connect shader to sg surface shader
+	connectAttr('%s.outColor' %shader ,'%s.surfaceShader' %shading_group)
+	#7. connect file texture node to shader's color
+	if(useTexture==True):
+		connectAttr('%s.outColor' %file_node, '%s.color' %shader)
+	#9. restore selection
+	select(target)
+	#10. return the completed shader
+	return shader
+
+def assignShader(shader):
+	hyperShade(a=shader)
+
+def quickShader(shaderType,shaderColor,useTexture):
+	shader = initShader(shaderType,shaderColor,useTexture)
+	assignShader(shader)
+
+def setColor(s,c):
+	cc = (float(c[0]) / 255.0, float(c[1]) / 255.0, float(c[2]) / 255.0)
+	setAttr(s + ".color", cc)
+
+quickShader("blinn",(255,0,0),False)
